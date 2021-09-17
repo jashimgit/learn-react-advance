cdfb5175c74d4f1a020ad0a8a4e143f8844d021e
jashimgit <jashim.ncn@gmail.com>
2021-09-17 10:07:10 +0600

    useReducer with useContext hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-// import Clock from './component/Clock';
-// import Forms from './component/Forms';
 import './App.css';
-import Counter from './useEffecthook/Counter';
+import ComponentA from './component/ComponentA';
+// import Counter from './component/Counter';
+import React, { createContext, useReducer } from 'react';
 
-import { useState } from 'react';
+export const counterContext = createContext();
+
+const initialState = {
+    count: 0,
+};
+
+function counterReducer(state, action) {
+    switch (action.type) {
+        case 'increment':
+            return { count: state.count + 1 };
+        case 'decrement':
+            return { count: state.count - 1 };
+
+        default:
+            return state;
+    }
+}
 
 function App() {
-    const [show, setshow] = useState(true);
+    const [state, dispatch] = useReducer(counterReducer, initialState);
     return (
         <div>
-            <div>{show && <Counter />}</div>
-            <p>
-                <button
-                    type='button'
-                    onClick={() => setshow((prevShow) => !prevShow)}
-                >
-                    {show ? 'Hide counter' : 'Show countter'}
-                </button>
-            </p>
-
-            
+            <h3>useReducer() hook </h3>
+            <hr />
+            {/* <Counter /> */}
+            <p>Counter: {state.count}</p>
+            <counterContext.Provider value={{dispatch}}>
+                <ComponentA />
+            </counterContext.Provider>
         </div>
     );
 }
